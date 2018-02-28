14f3136c177163a19cc2ea7ac045b234f85d22f9
Karl Beldan <karl.beldan+oss@gmail.com>
2018-02-28 22:22:56 +0000

    nandsim.sh: misc update
    - dyn peb_sz
    - retry ubiupdatevol vs EBUSY
    - compare the md5s
    
    Signed-off-by: Karl Beldan <karl.beldan+oss@gmail.com>

--- nandsim.sh
+++ nandsim.sh
@@ -1,26 +1,34 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
-LUBI_PRG=$(dirname $(realpath $0))/lubi
+#qemu="qemu-mips -L /usr/mips-linux-gnu"
+LUBI_PRG="$qemu $(dirname $(realpath $0))/lubi"
 
 modprobe nandsim parts="32" bbt=1
 modprobe gluebi
 
 rand_data=rand.dat
 vol="vol_0"
-mtd=$(sed -ne 's/^\([^:]*\):.*NAND sim.*0"$/\1/p' /proc/mtd)
+mtdline=($(grep "NAND simulator partition 0" /proc/mtd))
+mtd=${mtdline[0]%:}
+peb_sz=$((0x${mtdline[2]}))
 cdev=$(ubiattach -p /dev/$mtd -b 1 | sed -ne 's/.*number \([0-9]*\).*/\1/p')
 
 ubimkvol /dev/ubi$cdev -S 4 -N $vol -t static
 dd bs=23581 count=1 if=/dev/urandom of=$rand_data 2>/dev/null
-ubiupdatevol /dev/ubi0_0 $rand_data
+# Retry for EBUSY
+i=0; while [ $((i++)) -lt 3 ]; do ubiupdatevol /dev/ubi0_0 $rand_data && break; done
 
 nanddump --bb=dumpbad -n /dev/$mtd -f ${mtd}.dat
-
-$LUBI_PRG --ifile ${mtd}.dat --peb_sz $((16 << 10)) --vol $vol --ofile=${vol}.dat
+$LUBI_PRG --ifile ${mtd}.dat --peb_sz $peb_sz --vol $vol --ofile=${vol}.dat
 
 vol_mtd=/dev/$(sed -ne 's/^\([^:]*\):.*'$vol'"$/\1/p' /proc/mtd)
 
-for f in $rand_data $vol_mtd ${vol}.dat; do md5sum $f; done
+files="$rand_data $vol_mtd ${vol}.dat"
+i=0 md5s=(); for f in $files; do md5=$(md5sum $f); md5s[$i]=${md5%% *}; ((i++)); done
+[ ${md5s[0]}${md5s[0]} != ${md5s[1]}${md5s[2]} ] && {
+	echo XXX Bad md5s ${md5s[@]}
+	exit 1
+}
 
 rm -f $rand_data ${vol}.dat ${mtd}.dat
 ubidetach -d $cdev
